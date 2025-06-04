45b51dc31cbbaa9bdd468aebaa5c6a37a6907644
Grilo <matheusvsmdrive@gmail.com>
2025-06-04 17:20:18 -0300

    First commit trying to clone template from robbins23/admin-nextjs-daisyui
    Link to continue: https://github.com/robbins23/admin-dashboard-nextjs-typescript-daisyui-template

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,44 @@
+"use client";
+import Header from "@/components/dashboard/layout/Header";
+import { useRef } from "react";
+
+interface DashboardProps {
+  children: React.ReactNode;
+  params: { workspaceSlug: string };
+}
+
+export default function DashboardLayout({ children, params }: DashboardProps) {
+  const mainContentRef = useRef(null);
+
+  return (
+    <>
+      <div className="drawer lg:drawer-open">
+        <input
+          id="left-sidebar-drawer"
+          type="checkbox"
+          className="drawer-toggle"
+        />
+        <div className="drawer-content flex flex-col ">
+          <Header contentRef={mainContentRef} />
+          <main
+            className="flex-1 overflow-y-auto md:pt-4 pt-4 px-6  bg-base-200"
+            ref={mainContentRef}
+          >
+            {children}
+            <div className="h-16"></div>
+          </main>
+        </div>
+        {/* <LeftSidebar /> */}
+      </div>
+
+      {/* Right drawer - containing secondary content like notifications list etc.. */}
+      {/* <RightSidebar /> */}
+
+      {/* Notification layout container */}
+      {/* <ToastContainer /> */}
+
+      {/* Modal layout container */}
+      {/* <ModalLayout /> */}
+    </>
+  );
+}
